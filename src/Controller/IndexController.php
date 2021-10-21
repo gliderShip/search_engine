43811ab09808cbca1e8da67867bacfe31ba44704
Erin Hima <erinhima@gmail.com>
2021-10-21 14:08:25 +0200

    fb

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Service\Fiskalizimi;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends BaseController
+class IndexController extends AbstractController
 {
 
     /**
